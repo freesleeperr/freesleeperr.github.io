ef5480171421d62e979ad4dd7ec4ca154387d335
Reid <skyluckin@gmail.com>
2024-02-21 14:53:52 +0800

    init

--- src/app/dashboard/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <text className='bg-blue-500'>歪游戏</text>
-            <text></text>
-        </main>
-    )
-}
